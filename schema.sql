e2eb6e5a0c080b614c56012ef09b82f33176c31b
Kris-Mikael Krister <krismikael@protonmail.com>
2020-03-08 18:56:09 +0100

    Add timestamp columns to tables

--- schema.sql
+++ schema.sql
@@ -2,12 +2,16 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE surveys (
     id UUID NOT NULL DEFAULT uuid_generate_v4(), 
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
     owner_email text,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE responses (
     id UUID NOT NULL DEFAULT uuid_generate_v4(), 
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
     survey_id UUID,
     salary int,
     email text,
